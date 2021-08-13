bc4e2330394c51dd6003e44dd557421c8aee3e30
tyfah <tafadzwaallentyfah@gmail.com>
2021-08-13 10:36:42 +0200

    Initial Commit

---
+++ src/main/java/zw/co/afrosoft/MysqlLoginApplication.java
@@ -0,0 +1,13 @@
+package zw.co.afrosoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlLoginApplication.class, args);
+    }
+
+}
